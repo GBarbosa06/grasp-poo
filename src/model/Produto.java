0630f7a95e4b30bd442c58301866f8a959cbc719
Fernando Osório de Oliveira <141930264+Dewzin@users.noreply.github.com>
2025-11-12 13:42:52 -0300

    Produto.java
    Updated comments for clarity in the Produto class.

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,18 +1,18 @@
 package model;
-
+// Essa classe representa um produto, ou seja algo que pode ser vendido ou adicionado à um pedido.
 public class Produto {
     private String nome;
     private double preco;
-
+    // Quando criamos um novo produto, já informamos o nome e o preço
     public Produto(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
     }
-
+    // Retorna o nome do produto
     public String getNome() {
         return nome;
     }
-
+    // Retorna o preço do produto
     public double getPreco() {
         return preco;
     }
