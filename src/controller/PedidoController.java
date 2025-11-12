2e78827f79bfa47ab0a6b326f0f47b59bea51d60
Fernando Osório de Oliveira <141930264+Dewzin@users.noreply.github.com>
2025-11-12 00:18:33 -0300

    Refactor adicionarItem method in PedidoController

--- src/controller/PedidoController.java
+++ src/controller/PedidoController.java
@@ -2,22 +2,22 @@ package controller;
 
 import model.Pedido;
 import model.Produto;
-
+// Classe responsável por controlar as operações relacionadas à um pedido
 public class PedidoController {
     private Pedido pedidoAtual;
-
+    // Cria um novo pedido
     public void criarPedido() {
         pedidoAtual = new Pedido();
         System.out.println("Novo pedido criado!");
     }
-
+    // Adiciona um produto ao pedido atual
     public void adicionarItem(Produto produto, int quantidade) {
-        if (pedidoAtual != null) {
-            pedidoAtual.adicionarItem(produto, quantidade);
-            System.out.println("Item adicionado: " + produto.getNome());
+        if (pedidoAtual != null) { 
+            pedidoAtual.adicionarItem(produto, quantidade); 
+            System.out.println("Item adicionado: " + produto.getNome()); 
         }
     }
-
+    // Mostra um resumo final do pedido
     public void finalizarPedido() {
         if (pedidoAtual != null) {
             pedidoAtual.exibirResumo();
