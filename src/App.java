4428dfc48408bbec5d83c09f0395281133f788d8
Fernando Osório de Oliveira <141930264+Dewzin@users.noreply.github.com>
2025-11-12 14:00:47 -0300

    App.java

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
         PedidoController controller = new PedidoController();
-
+        // Menu inicial
         System.out.println("=== SISTEMA DE PEDIDOS ===");
         System.out.println("1 - Criar pedido manualmente");
         System.out.println("2 - Executar teste automático");
@@ -19,27 +19,30 @@ public class App {
 
         switch (opcao) {
             case 1:
+                 // Opção de criar um pedido manualmente
                 controller.criarPedido();
 
                 while (true) {
+                    // Pede o nome do produto
                     System.out.print("\nDigite o nome do produto (ou 'sair' para finalizar): ");
                     String nome = sc.nextLine();
+                     // Se o usuário digitar "sair", o loop é encerrado
                     if (nome.equalsIgnoreCase("sair")) break;
-
+                     // Pede o preço e a quantidade do produto
                     System.out.print("Preço do produto: ");
                     double preco = sc.nextDouble();
 
                     System.out.print("Quantidade: ");
                     int qtd = sc.nextInt();
-                    sc.nextLine(); // limpar buffer
-
+                    sc.nextLine();
+                    // Cria um novo produto com as informações digitadas
                     Produto produto = new Produto(nome, preco);
                     controller.adicionarItem(produto, qtd);
                 }
-
+                // Mostra o resumo final do pedido
                 controller.finalizarPedido();
                 break;
-
+            // Opção de teste automático
             case 2:
                 Produto p1 = new Produto("Mouse", 80.0);
                 Produto p2 = new Produto("Teclado", 120.0);
@@ -51,6 +54,7 @@ public class App {
                 break;
 
             default:
+                // Caso o usuário digite uma opção inválida
                 System.out.println("Opção inválida!");
         }
 
